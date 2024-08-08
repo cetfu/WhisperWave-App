99214102a4c96d0e91fd511d4c9941d9588a8c89
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2024-08-08 23:07:35 +0300

    feat(api route): generate send message endpoint

--- app/api/threads/[qr]/route.ts
+++ app/api/threads/[qr]/route.ts
@@ -1,8 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
+import {API_URL} from "@/constants";
 
 export async function GET(request: NextRequest, {params}: { params: { qr: string } }) {
 
-    const res = await fetch("http://localhost:3200/threads/" + params.qr)
+    const res = await fetch(API_URL + "/threads/" + params.qr)
     const data = await res.json()
 
     return new NextResponse(JSON.stringify(data), {
@@ -10,5 +11,22 @@ export async function GET(request: NextRequest, {params}: { params: { qr: string
     });
 }
 
-export async function POST(request: Request) {
+export async function POST(request: Request, {params}: { params: { qr: string } }) {
+    const requestData = await request.json()
+
+    const res = await fetch(API_URL + "/threads/" + params.qr, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            message: requestData.message
+        })
+    })
+
+    const data = await res.json()
+
+    return new NextResponse(JSON.stringify(data), {
+        status: 200,
+    });
 }
