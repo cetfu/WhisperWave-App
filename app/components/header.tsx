c2bc49d5c9ee99afb303cd3eac37f7f545687143
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2024-08-08 15:50:48 +0300

    fix(header): added return to home

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,14 @@
 import {ModeToggle} from "@/components/toggle-theme";
+import Link from "next/link";
 
 export default function Header(){
     return (
         <header className={"w-full h-20 flex justify-between items-center px-5 bg-primary"}>
             <div>
                 <h1 className={"text-xl font-bold text-accent"}>
-                    WhisperWave
+                    <Link href={"/"}>
+                        WhisperWave
+                    </Link>
                 </h1>
             </div>
             {/*<ModeToggle />*/}
