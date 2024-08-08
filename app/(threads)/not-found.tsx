fb8eeb72f2608823fa3e8c365f0e083e339d904d
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2024-08-08 23:05:57 +0300

    build(qr layout): add 404 to qr layout

---
+++ app/(threads)/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+    return (
+        <div className={"grow flex justify-center items-center"}>
+            <h1 className={"font-bold text-2xl"}>404 | Thread not found</h1>
+        </div>
+    );
+}
+
+export default NotFound;
